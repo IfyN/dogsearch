9818f7eea3df55ba181887e6669914c333e7430d
IfyN <ifynwosu0@gmail.com>
2023-02-08 08:35:10 +0100

    - fetch subbreed images - fetch subbreed names

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,39 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Breed: {{ $route.params.breed }}</h1>
+    <h2> Sub Breeds: </h2>
+    <ul>
+      <li v-for="sub in subBreeds" :key="sub"> {{ sub }} </li>
+    </ul>
+    <img v-for="image in subBreedImages" :key="image" :src="image" />
   </div>
 </template>
+<script>
+// import axios from "axios";
+
+import axios from 'axios';
+
+
+export default {
+  name: "About",
+  data() {
+    return {
+      subBreedImages: [],
+      subBreeds: [],
+    }
+  },
+
+  async mounted() {
+    const { breed } = this.$route.params
+    const breedImages = await axios.get(`https://dog.ceo/api/breed/${breed}/images/random/5`)
+    const subBreedImages = breedImages.data.message
+    this.subBreedImages = subBreedImages;
+    // console.log({ subBreedImages })
+
+    const subBreeds = await axios.get(`https://dog.ceo/api/breed/${breed}/list`)
+    this.subBreeds = subBreeds.data.message[breed]
+
+  }
+}
+
+</script>
