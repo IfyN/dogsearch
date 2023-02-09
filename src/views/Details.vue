79549b2e278bf5c22435e017e5cdcdb50dcbf7af
IfyN <ifynwosu0@gmail.com>
2023-02-09 14:31:34 +0100

    change about filename to 'details'

---
+++ src/views/Details.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="body">
+    <h1>Breed: {{ $route.params.breed }}</h1>
+    <h2> Sub Breeds: </h2>
+    <ul>
+      <li v-for="sub in subBreeds" :key="sub"> {{ sub }} </li>
+    </ul>
+    <div class="infobox">
+      <div class="infobox_content">
+        <img class="img_style" v-for="image in subBreedImages" :key="image" :src="image" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+
+
+export default {
+  name: "Details",
+
+  data() {
+    return {
+      subBreedImages: [],
+      subBreeds: [],
+    }
+  },
+
+  async mounted() {
+    const { breed } = this.$route.params
+    const breedImages = await axios.get(`https://dog.ceo/api/breed/${breed}/images/random/3`)
+    const subBreedImages = breedImages.data.message
+    this.subBreedImages = subBreedImages;
+
+    const subBreeds = await axios.get(`https://dog.ceo/api/breed/${breed}/list`)
+    this.subBreeds = subBreeds.data.message[breed];
+  }
+}
+
+</script>
+
+<style scoped>
+body {
+  padding: 0;
+  margin: 0;
+  font-family: 'Trispace', sans-serif;
+  line-height: 1.6;
+  font-size: 18px;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.infobox_content {
+  text-align: center;
+  border: none;
+  padding-top: 5px;
+}
+
+.infobox {
+  width: 100%;
+  max-width: 940px;
+  margin: 0 auto;
+  display: grid;
+  justify-content: center;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 300px));
+  gap: 20px;
+  margin-top: 20px;
+}
+
+.img_style {
+  width: 300px;
+  height: 300px;
+}
